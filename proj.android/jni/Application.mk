5200619e641aa3a011426181f4f93f2d962d3be4
Fedor Shubin <shubin.fedor@gmail.com>
2014-06-12 18:08:34 +0400

    turn off uninitialized error in Android

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -DCOCOS2D_DEBUG=1 -DSOOMLA_DEBUG=1
-APP_CFLAGS += -Wno-error=format-security -Werror=uninitialized
+APP_CFLAGS += -Wno-error=format-security -Wno-error=uninitialized
 
 APP_ABI := armeabi
 APP_PLATFORM := android-8
